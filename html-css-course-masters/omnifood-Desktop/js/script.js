45e3f89e59b9d2806445e93858c34425862e031d
dong-geon-Lee <ch9716@naver.com>
2022-04-10 00:08:00 +0900

    course done

---
+++ html-css-course-masters/omnifood-Desktop/js/script.js
@@ -0,0 +1,89 @@
+// const yearEl = document.querySelector(".year");
+// const currentYear = new Date().getFullYear();
+
+// yearEl.textContent = currentYear;
+
+// const checkFlexGap = () => {
+//   let flex = document.createElement("div");
+//   flex.style.display = "flex";
+//   flex.style.flexDirection = "column";
+//   flex.style.rowGap = "1px";
+
+//   flex.appendChild(document.createElement("div"));
+//   flex.appendChild(document.createElement("div"));
+
+//   document.body.appendChild(flex);
+
+//   let isSupported = flex.scrollHeight === 1;
+
+//   flex.parentNode.removeChild(flex);
+
+//   console.log(isSupported);
+
+//   if (!isSupported) {
+//     document.body.classList.add("no-flexbox-gap");
+//   }
+// };
+
+// checkFlexGap();
+
+// Sticky navigation
+
+const sectionHeroEl = document.querySelector(".section-hero");
+
+const obs = new IntersectionObserver(
+  (entries) => {
+    const ent = entries[0];
+    console.log(ent);
+
+    if (ent.isIntersecting === false) {
+      document.body.classList.add("sticky");
+    }
+
+    if (ent.isIntersecting === true) {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHeroEl);
+
+const btnNavEl = document.querySelector(".btn-mobile-nav");
+const headerEl = document.querySelector(".header");
+
+btnNavEl.addEventListener("click", () => {
+  headerEl.classList.toggle("nav-open");
+});
+
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach((link) => {
+  link.addEventListener("click", (e) => {
+    e.preventDefault();
+
+    const href = link.getAttribute("href");
+
+    // Scoll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    //  Scrool to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      console.log(sectionEl);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile navigation
+    if (link.classList.contains("main-nav-link")) {
+      headerEl.classList.remove("nav-open");
+    }
+  });
+});
